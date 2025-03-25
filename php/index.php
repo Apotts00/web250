fc731ea489ed07d117af711620c10f81e61d573c
Apotts00 <apotts00@email.cpcc.edu>
2025-03-25 11:38:21 -0400

    Update and rename web250 to index.php

---
+++ php/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adrienne Love 🐞 Ambitious Ladybug</title>
+    <!-- Link to CSS files -->
+    <link href="styles/default.css" rel="stylesheet" type="text/css"> 
+	<script src="https://lint.page/kit/880bd5.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<h2>Home</h2>
+    <!-- Sectioning elements -->
+  <div data-include="components/header.html"></div>
+	<figure>
+   <img src="images/ladylove.png" alt="Beautiful Me ;-)" width="300" height="300">
+   <figcaption><em>Charlotte, NC ❤️</em></figcaption>
+   </figure>
+        <p>Enjoy Learning About Me!!!</p>
+    
+	<p>
+	Our mission is to create a dynamic and fun learning environment by partnering with business owners to offer engaging, 
+	interactive classes that inspire and educate the public. We’re dedicated to blending knowledge with excitement, 
+	cultivating a positive vibe where learning is not only impactful but enjoyable.
+	Together, we empower individuals to grow, connect, and thrive in an atmosphere of creativity and collaboration."
+	To learn more about the owner, please check out my <a href="introduction.html">introduction</a>&nbsp;page. 
+	</p>   
+	
+ <div data-include="components/footer.html"></div>
+	<script src="scripts/HTMLInclude.js"></script>
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
