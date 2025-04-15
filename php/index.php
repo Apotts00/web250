ca20b6c53471cfb00576bf31f5ae53d3394ffd8e
Apotts00 <apotts00@email.cpcc.edu>
2025-04-14 22:59:49 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -50,10 +50,24 @@
     <h2><?php echo strtoupper($pageTitle)?></h2> 
     <?php include($pagePath)?>
 </main>
-	<?php include 'components/footer.html' ?>
-</body>
-</html>
-
+	 <footer>
+        <nav>
+            <a href="https://github.com/Apotts00">GitHub</a>&nbsp;
+            <a href="https://apotts00.github.io/">GitHub.io</a>&nbsp;
+            <a href="https://apotts00.github.io/web115/">WEB115.io</a>&nbsp;
+            <a href="https://apotts00.github.io/web215/">WEB215.io</a>&nbsp;
+            <a href="https://apotts00.github.io/web250/">WEB250.io</a>&nbsp;
+            <a href="https://www.freecodecamp.org/apotts00">freeCodeCamp</a>&nbsp;
+            <a href="https://www.codecademy.com/profiles/adriennepotts">Codecademy</a>&nbsp;
+            <a href="https://jsfiddle.net/u/apotts00/fiddles/">JSFiddle</a>&nbsp;    
+            <a href="https://www.linkedin.com/in/adrienne-potts/">LinkedIn</a>&nbsp;
+        </nav>
+    <p>Site built by <a href="https://apotts00.github.io/web250/adriennelove.html"><em>Adrienne Love</em></a>. All rights reserved. ©2025 Certified in 
+        <a class="certification" href="https://www.freecodecamp.org/certification/apotts00/responsive-web-design" target="_blank"><em>RWD</em></a> and 
+        <a class="certification" href="https://www.freecodecamp.org/certification/apotts00/javascript-algorithms-and-data-structures-v8" target="_blank"><em>JADS</em></a>
+    </p>
+        <p>Play Safe, Laugh Hard, Grow Together!</p>
+    </footer>
 
 
 
