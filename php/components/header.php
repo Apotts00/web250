5dde014d6853c1a33df319be1804d8f5722332e0
Apotts00 <apotts00@email.cpcc.edu>
2025-05-09 00:11:30 -0400

    Update header.php

--- php/components/header.php
+++ php/components/header.php
@@ -1,22 +1,21 @@
 <header>
-<h1>Adrienne Love's Ambitious Ladybug 🐞 WEB250 🐞 Static Site</h1>
+<h1>Adrienne Love's Ambitious Ladybug 🐞 WEB250 🐞 PHP Site</h1>
 
 <nav>
   <ul class="nav-menu">
-    <li><a href="index.html">Home</a></li>
-    <li><a href="introduction.html">Introduction</a></li>
-    <li><a href="contract.html">Contract</a></li>
-    <li><a href="intro_form.html">Intro Form</a></li>
-    <li><a href="fizzbuzz.html">Fizz Buzz</a></li>
-    <li><a href="http://alove.great-site.net//">PHP Site</a></li>
+    <li><a href="index.php?p=intro_form">Home</a></li>
+    <li><a href="index.php?p=introduction.php">Introduction</a></li>
+    <li><a href="index.php?p=contract.php">Contract</a></li>
+    <li><a href="index.php?p=intro_form.php">Intro Form</a></li>
+    <li><a href="index.php?p=fizzbuzz.php">Fizz Buzz</a></li>
+    <li><a href="https://apotts00.github.io/web250/">Static Site</a></li>
     <li class="dropdown">
       
       <a href="#">External Pages</a>
       <ul class="dropdown-menu">
-        <li><a href="multipage_sites/superduper_static/index.htm">Superduper Static</a></li>
-        <li><a href="http://alove.great-site.net/multipage_sites/superduper_php/">Superduper PHP</a></li>
-        <li><a href="http://alove.great-site.net//joyofphp/joyphp/src/">Joy of PHP</a></li>
-        <li><a href="http://alove.great-site.net//joyofphp/joyphp/src/samsusedcars.html">Sam's Used Cars</a></li>
+        <li><a href="http://alove.great-site.net/multipage_sites/superduper_static/index.htm">Superduper Static</a></li>
+        <li><a href="multipage_sites/superduper_php/">Superduper PHP</a></li>
+        <li><a href="joyofphp/src/">Joy of PHP</a></li>
         <li><a href="http://alove.great-site.net/carapp/index.php?">Ambitious Ladybug's Used Cars</a></li>
       </ul>
     </li>
